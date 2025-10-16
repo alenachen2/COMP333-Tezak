c9c432a50e36ad893126413068012d87cc532b0c
Saakshi Challa <schalla@wesleyan.edu>
2025-10-15 23:39:56 -0400

    cell id. user test

---
+++ cell_identification_test.py
@@ -0,0 +1,17 @@
+#user test
+
+from cell_identification import classify_cell, count_cells
+
+def test_classify_cell_red():
+    cell = {"color": "red"}
+    assert classify_cell(cell) == "Cell Identified"
+
+def test_classify_notcell():
+    cell = {"color": "blue"} 
+    assert classify_cell(cell) == "Cell Not Identified"
+
+def test_count_cells():
+    cells = [{"color": "red"}, {"color": "blue"}, {"color": "red"}]
+    result = count_cells(cells)
+    assert result == {"Cells Identified": 2, "Cells Not Identified": 1}
+
