99158308320fdec95be87bdefbb464208199466b
Saakshi Challa <schalla@wesleyan.edu>
2025-10-15 23:22:40 -0400

    user test functions

--- cell_identification.py
+++ cell_identification.py
@@ -1,1 +1,16 @@
+#User test function 
+def classify_cell(cell): 
+    if cell["color"] == "red": 
+        return "Cell Identified"
+    else: 
+        return "Not a Cell"
 
+def count_cells(cell_list): 
+    count_cell = 0 
+    count_notcell = 0
+    for cell in cell_list: 
+        if classify_cell(cell) == "Cell Identified": 
+            count_cell += 1
+        else: 
+            count_notcell += 1
+    return{"Cells Identified": count_cell, "Not a Cell": count_notcell}
