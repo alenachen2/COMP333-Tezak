108cc2c3a86507188ae663b0e8e8f7aeef47f79f
acurielwes <acuriel@wesleyan.edu>
2025-10-28 15:40:24 -0400

    reverting back, no tk

--- file_retrieval2.py
+++ file_retrieval2.py
@@ -1,22 +1,6 @@
 from PIL import Image 
-from tkinter import Tk, filedialog
 
 image_path = "path_name"
-
-# Hide the root window
-Tk().withdraw()
-
-# Ask the user to select an image file
-file_path = filedialog.askopenfilename(
-    title="Select an image file",
-    filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")]
-)
-
-if file_path:
-    print("Selected image path:", file_path)
-else:
-    print("No file selected.")
-
 
 try:
     img = Image.open(image_path)
