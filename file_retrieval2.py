d3756e7f9eeba4b3e266adfece82563e192955c2
acurielwes <acuriel@wesleyan.edu>
2025-10-28 16:08:21 -0400

    added inputting file via terminal

--- file_retrieval2.py
+++ file_retrieval2.py
@@ -1,13 +1,16 @@
 from PIL import Image 
 
-image_path = "path_name"
+#user manually opens this file and pastes file name
+image_path : str 
 
 try:
-    img = Image.open(image_path)
-
-    img.show()
+    image_path = input("paste your file path here: ")
 
 except FileNotFoundError:
-     print(f"Error: The file '{image_path}' was not found.")
+     print(f"Error: The file '{image_path}' was not found. Run the program again.")
 except IOError:
     print(f"Error: Could not open or identify the image file '{image_path}'.")
+
+img = Image.open(image_path)
+
+img.show()
