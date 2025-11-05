2c041d5e2d0e1c5ab2401dbb47f0bf6c73472515
acurielwes <acuriel@wesleyan.edu>
2025-11-04 21:07:22 -0500

    added get_file_path function

--- file_retrieval2.py
+++ file_retrieval2.py
@@ -1,16 +1,18 @@
 from PIL import Image 
 
-#user manually opens this file and pastes file name
-image_path : str 
+def get_file_path():
+    """prompts user to copy the desired image's filepath and returns it"""
+    imagePath : str 
 
-try:
-    image_path = input("paste your file path here: ")
+    try:
+        imagePath = input("paste your file path here: ")
 
-except FileNotFoundError:
-     print(f"Error: The file '{image_path}' was not found. Run the program again.")
-except IOError:
-    print(f"Error: Could not open or identify the image file '{image_path}'.")
+    except FileNotFoundError:
+        print(f"Error: The file '{imagePath}' was not found. Try again.")
+        get_file_path()
+    except IOError:
+        print(f"Error: Could not open or identify the image file '{image_path}'.")
 
-img = Image.open(image_path)
+    return imagePath
 
-img.show()
+#img = Image.open(image_path)
