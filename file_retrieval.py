df1a13f4aaabfd95d6c8bebb2e2d35342ed79c8b
acurielwes <acuriel@wesleyan.edu>
2025-11-04 21:34:07 -0500

    Update file_retrieval.py
    fixed variables to snake_case

--- file_retrieval.py
+++ file_retrieval.py
@@ -2,17 +2,17 @@ from PIL import Image
 
 def get_file_path():
     """prompts user to copy the desired image's filepath and returns it"""
-    imagePath : str 
+    image_path : str 
 
     try:
-        imagePath = input("paste your file path here: ")
+        image_path = input("paste your file path here: ")
 
     except FileNotFoundError:
-        print(f"Error: The file '{imagePath}' was not found. Try again.")
+        print(f"Error: The file '{image_path}' was not found. Try again.")
         get_file_path()
     except IOError:
-        print(f"Error: Could not open or identify the image file '{imagePath}'.")
+        print(f"Error: Could not open or identify the image file '{image_path}'.")
 
-    return imagePath
+    return image_path
 
 #img = Image.open(image_path)
