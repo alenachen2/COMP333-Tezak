040902f2d72c8fd383d47f301fbaf60dcb23f443
acurielwes <acuriel@wesleyan.edu>
2025-11-04 21:21:13 -0500

    delete duplicate file, added functionaility to main file

--- file_retrieval.py
+++ file_retrieval.py
@@ -1,1 +1,18 @@
+from PIL import Image 
 
+def get_file_path():
+    """prompts user to copy the desired image's filepath and returns it"""
+    imagePath : str 
+
+    try:
+        imagePath = input("paste your file path here: ")
+
+    except FileNotFoundError:
+        print(f"Error: The file '{imagePath}' was not found. Try again.")
+        get_file_path()
+    except IOError:
+        print(f"Error: Could not open or identify the image file '{image_path}'.")
+
+    return imagePath
+
+#img = Image.open(image_path)
