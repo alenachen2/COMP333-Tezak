9639839f6aee9d3b343bbc94a4d086a65a5e31e8
Alena <achen04@wesleyan.edu>
2025-11-04 20:21:08 -0500

    moved file from main branch to cell_identification branch

---
+++ cell_identification_test.py
@@ -0,0 +1,16 @@
+#user test
+
+from cell_identification import classify_cell, count_cells
+
+def test_classify_cell_red():
+    cell = {"color": "red"}
+    assert classify_cell(cell) == "Cell Identified"
+
+def test_classify_notcell():
+    cell = {"color": "blue"} 
+    assert classify_cell(cell) == "Not a Cell"
+
+def test_count_cells():
+    cells = [{"color": "red"}, {"color": "blue"}, {"color": "red"}]
+    result = count_cells(cells)
+    assert result == {"Cells Identified": 2, "Cells Not Identified": 1}
