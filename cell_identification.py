656ced513bfbf194f6e9fb1ee97fce9192febb72
Alena <achen04@wesleyan.edu>
2025-11-24 17:41:48 -0500

    removed commented out function calls

--- cell_identification.py
+++ cell_identification.py
@@ -117,26 +117,3 @@ def display_results(img_array, masks_array, flows_array):
         ax.set_title(color_labels[i] + " Channel Segmentation")
         plt.tight_layout()
         plt.show()
-
-
-# #uisng for easy testing for now.
-# img_path = "/Users/alenachen/Downloads/images/9.jpg"
-# #img_path = get_file_path()
-# img = cv2.imread(img_path).copy()
-
-# b, g, r = split_channels(img)
-# imgs_clean_array = clean(r, g, b)
-
-# with yaspin(text="Identifying cells...", color="yellow") as spinner:
-#     model = create_model()
-#     masks, flows, styles = segment(model, imgs_clean_array)
-
-# red_ROIs = extract_ROI('red', masks)
-# green_ROIs = extract_ROI('green', masks)
-# blue_ROIs = extract_ROI('blue', masks)
-
-# print(f"Red cells detected: {len(red_ROIs) - 1}"   
-#     f"\nGreen cells detected: {len(green_ROIs) - 1}"   
-#     f"\nBlue of cells detected: {len(blue_ROIs) - 1}") 
-
-# display_results(imgs_clean_array, masks, flows)
