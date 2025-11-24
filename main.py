d0f12e2fb16a9b34d5c0b57c31b56d80c490a6cb
Alena <achen04@wesleyan.edu>
2025-11-24 16:40:13 -0500

    moved function calls from cell_identification.py and put them in main.py

--- main.py
+++ main.py
@@ -9,26 +9,25 @@ from cell_identification import segment
 from cell_identification import extract_ROI
 from cell_identification import display_results
 
-
-
+if __name__ == "__main__":
 #uisng for easy testing for now.
-img_path = "/Users/alenachen/Downloads/images/9.jpg"
+    img_path = "/Users/alenachen/Downloads/images/9.jpg"
 #img_path = get_file_path()
-img = cv2.imread(img_path).copy()
+    img = cv2.imread(img_path).copy()
 
-b, g, r = split_channels(img)
-imgs_clean_array = clean(r, g, b)
+    b, g, r = split_channels(img)
+    imgs_clean_array = clean(r, g, b)
 
-with yaspin(text="Identifying cells...", color="yellow") as spinner:
-    model = create_model()
-    masks, flows, styles = segment(model, imgs_clean_array)
+    with yaspin(text="Identifying cells...", color="yellow") as spinner:
+        model = create_model()
+        masks, flows, styles = segment(model, imgs_clean_array)
 
-red_ROIs = extract_ROI('red', masks)
-green_ROIs = extract_ROI('green', masks)
-blue_ROIs = extract_ROI('blue', masks)
+    red_ROIs = extract_ROI('red', masks)
+    green_ROIs = extract_ROI('green', masks)
+    blue_ROIs = extract_ROI('blue', masks)
 
-print(f"Red cells detected: {len(red_ROIs) - 1}"   
-    f"\nGreen cells detected: {len(green_ROIs) - 1}"   
-    f"\nBlue of cells detected: {len(blue_ROIs) - 1}") 
+    print(f"Red cells detected: {len(red_ROIs) - 1}"   
+        f"\nGreen cells detected: {len(green_ROIs) - 1}"   
+        f"\nBlue of cells detected: {len(blue_ROIs) - 1}") 
 
-display_results(imgs_clean_array, masks, flows)
+    display_results(imgs_clean_array, masks, flows)
