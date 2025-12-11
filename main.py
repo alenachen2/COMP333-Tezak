37720d6663099e9ec670f81ed5306e133cc7b13d
Alena <achen04@wesleyan.edu>
2025-12-10 22:59:14 -0500

    called new classify_cells function

--- main.py
+++ main.py
@@ -8,25 +8,31 @@ from cell_identification import create_model
 from cell_identification import segment
 from cell_identification import extract_ROI
 from cell_identification import display_results
+from cell_identification import classify_cells_by_color
 from cell_counting import count
+import numpy as np
 
 if __name__ == "__main__":
     img_path = get_file_path()
-    img = cv2.imread(img_path).copy()
-
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     b, g, r = split_channels(img)
-    imgs_clean_array = clean(r, g, b)
 
     with yaspin(text="Identifying cells...", color="yellow") as spinner:
         model = create_model()
-        masks, flows, styles = segment(model, imgs_clean_array)
+        #masks, flows, styles = segment(model, imgs_clean_array)
 
-    red_ROIs = extract_ROI('red', masks)
-    green_ROIs = extract_ROI('green', masks)
-    blue_ROIs = extract_ROI('blue', masks)
+        masks, flows, styles = model.eval(
+            img,
+            flow_threshold=0.4,
+            cellprob_threshold=0.0,
+            normalize = True, 
+            diameter = None
+        )
+    all_counts = classify_cells_by_color(img, masks)
 
-    print("Red cells detected: " + str(count(red_ROIs)))
-    print("Green cells detected: " + str(count(green_ROIs)))
-    print("Blue cells detected: " + str(count(blue_ROIs)))
-
-    display_results(imgs_clean_array, masks, flows)
+    print("Total cells detected: " + str(count(np.unique(masks))))
+    print("Red cells detected: " + str(all_counts["red"]))
+    print("Green cells detected: " + str(all_counts["green"]))
+    print("Blue cells detected: " + str(all_counts["blue"]))
+   
