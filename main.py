871aeaf1119ff1f5d1a9109aec70ffa685404a23
Alena <achen04@wesleyan.edu>
2025-12-13 20:39:49 -0500

    segmentation for multiple images occurs

--- main.py
+++ main.py
@@ -12,30 +12,30 @@ from multiple_files import multiple_images
 import numpy as np
 
 if __name__ == "__main__":
+    
     img_array = multiple_images()
 
     print("Processing image...")
 
-    with yaspin(text="Identifying cells...", color="yellow") as spinner:
-    
-        b, g, r = split_channels(img)
+    for img in img_array:
 
-        model = create_model()
-
-        masks, flows, styles = model.eval(
-            img,
-            flow_threshold=0.4,
-            cellprob_threshold=0.0,
-            normalize = True, 
-            diameter = None
-        )
-
-        all_counts = classify_cells_by_color(img, masks)
+        with yaspin(text="Identifying cells...", color="yellow") as spinner:
         
-    print("Cell segmentation complete. Extracting color ROIs...")
-    print("Total cells detected: " + str(count(np.unique(masks))))
-    print("Red cells detected: " + str(all_counts["red"]))
-    print("Green cells detected: " + str(all_counts["green"]))
-    print("Blue cells detected: " + str(all_counts["blue"]))
-    display_results(img, masks, flows)
-   
+            model = create_model()
+
+            masks, flows, styles = model.eval(
+                img,
+                flow_threshold=0.4,
+                cellprob_threshold=0.0,
+                normalize = True, 
+                diameter = None
+            )
+
+            all_counts = classify_cells_by_color(img, masks)
+        
+        print("Cell segmentation complete. Extracting color ROIs...")
+        print("Total cells detected: " + str(count(np.unique(masks))))
+        print("Red cells detected: " + str(all_counts["red"]))
+        print("Green cells detected: " + str(all_counts["green"]))
+        print("Blue cells detected: " + str(all_counts["blue"]))
+        display_results(img, masks, flows)
