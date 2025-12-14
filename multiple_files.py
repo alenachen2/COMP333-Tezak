2a5efe4041586e29b124a733489862126aa18ed7
Alena <achen04@wesleyan.edu>
2025-12-13 20:23:44 -0500

    function that takes in multiple images instead of just one

---
+++ multiple_files.py
@@ -0,0 +1,26 @@
+from file_retrieval import get_file_path
+import cv2
+import numpy as np
+
+def multiple_images():
+    imgs_array = []
+    all_paths = []
+    while True:
+        img_path = get_file_path()
+        if img_path in all_paths:
+            add = input("This image has already been added. Do you want to add it again? Y or N\n").strip().upper()
+            if add == 'Y':
+                img = cv2.imread(img_path)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                imgs_array.append(img)
+            else: 
+                print ("Image was not added.")
+        else:
+            img = cv2.imread(img_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            imgs_array.append(img)
+            all_paths.append(img_path)
+        again = input("Would you like to add another file? Y or N\n").strip().upper()
+        if again == 'N':
+            break
+    return imgs_array
